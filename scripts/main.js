1aebf5174ae7a92b3789c2e60c56bd44502aaed6
Robin <swesson92@gmail.com>
2019-10-06 16:17:05 +0200

    some jQuery code

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,29 @@
+
+let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/plays.png') {
+      myImage.setAttribute ('src','images/station.jpg');
+    } else {
+      myImage.setAttribute ('src','images/plays.png');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'This shit is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
